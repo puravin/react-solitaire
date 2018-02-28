6f8cedccdacd326e507ae61786b41f459867ed98
Puravin <psivaganam01@gmail.com>
2018-02-28 16:39:55 +1100

    configure build for production

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,44 +1,60 @@
 var path = require('path');
 var webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: [
-    './src/index'
+    path.resolve(__dirname, '..', 'src/index')
   ],
   output: {
-      filename: 'index.js',
-      path: path.join(__dirname, '..', '/lib/'),
-      publicPath: '/lib/',
-      library: 'react-solitaire',
-      libraryTarget: 'umd'
+    filename: 'js/index.js',
+    path: path.join(__dirname, '..', '/build/'),
+    publicPath: './',
+    library: 'react-solitaire',
+    libraryTarget: 'umd'
   },
   plugins: [
     new webpack.DefinePlugin({
-        'process.env.NODE_ENV': '"production"'
+      'process.env.NODE_ENV': '"production"'
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html', // Move the index.html file...
+      minify: { // Minifying it while it is parsed
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true
     }),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
-          warnings: false
+        warnings: false
       }
     })
   ],
   resolve: {
     extensions: ['', '.jsx', '.js', '.json'],
-    modulesDirectories: ['node_modules', 'src'],
+    modulesDirectories: ['node_modules', 'src']
   },
   module: {
     loaders: [{
-        test: /\.scss$/,
-        loaders: ["style", "css", "sass"] 
-      }, {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      }, {
-        test: /\.(png|jpg|svg)$/,
-        loader: 'url-loader?limit=1048576'
-      }
-    ]
+      test: /\.scss$/,
+      loaders: ['style', 'css', 'sass']
+    }, {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.(png|jpg|svg)$/,
+      loader: 'url-loader?limit=1048576'
+    }]
   }
 };
